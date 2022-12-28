fee3e3f004c0e5d356479cbb9f1947808d5d394f
luiz <lguilhermefl@gmail.com>
2022-12-28 19:44:14 -0300

    feat: create config

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+  jsonCID: "QmNe7EebKaNuRoN2ov9nMuwHXQvXbYXCLM9W1nDBxnqLgL", // Metadata CID from a collection
+  collectionName: "yaypegs", // It's gonna the collection folder name
+  collectionSize: 10000, // Collection's total size
+  pageTimeout: 1500, // Maximun time to wait for each page to load. If you keep getting errors you can change this number to a bigger one
+};
+
+export default config;
