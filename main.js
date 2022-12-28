c13751bcf3851bcbfd8ee2a7c8f66de102013c17
luiz <lguilhermefl@gmail.com>
2022-12-28 19:46:33 -0300

    feat: add init puppeteer

--- main.js
+++ main.js
@@ -16,4 +16,15 @@ function createFolders() {
   }
 }
 
+async function initPuppeteer() {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  const ua =
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36";
+  await page.setExtraHTTPHeaders({ "Accept-Language": "en-US,en;q=0.9" });
+  await page.setUserAgent(ua);
+
+  return { browser, page };
+}
+
 createFolders();
