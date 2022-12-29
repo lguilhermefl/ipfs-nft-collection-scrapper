d24a8d509472d9a321d1512bd9f85d11695dd1cc
Luiz Guilherme Florentino <luizguilherme77@gmail.com>
2022-12-29 15:19:04 -0300

    docs: correct comment

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   jsonCID: "QmNe7EebKaNuRoN2ov9nMuwHXQvXbYXCLM9W1nDBxnqLgL", // Metadata CID from a collection
-  collectionName: "yaypegs", // It's gonna the collection folder name
+  collectionName: "yaypegs", // It's gonna be the collection folder name
   collectionSize: 10000, // Collection's total size
   pageTimeout: 1500, // Maximun time to wait for each page to load. If you keep getting errors you can change this number to a bigger one
 };
