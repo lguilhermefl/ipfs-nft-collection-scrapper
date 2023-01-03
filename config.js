cf3acee3e30828fb4b5532d90741039905d737b3
luiz <lguilhermefl@gmail.com>
2023-01-03 16:40:40 -0300

    feat: add first and last edition variables

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 const config = {
   jsonCID: "QmNe7EebKaNuRoN2ov9nMuwHXQvXbYXCLM9W1nDBxnqLgL", // Metadata CID from a collection
   collectionName: "yaypegs", // It's gonna be the collection folder name
+  firstEditionId: 1, // First edition from collection
+  lastEditionId: 10000, // Last Edition from collection
   collectionSize: 10000, // Collection's total size
   pageTimeout: 1500, // Maximum time to wait for each page to load. If you keep getting errors you can change this number to a bigger one
 };
